0bff50b38fd67f8d5351c34094f65b52d33ded3f
dwiprayogok <dwiprayogok@gmail.com>
2025-04-14 17:33:39 +0700

    info alamat

--- resources/views/siswa/views/ProfileSiswa.blade.php
+++ resources/views/siswa/views/ProfileSiswa.blade.php
@@ -69,7 +69,9 @@
 
       <div class="sm:col-span-2">
         <label for="address" class="block mb-2 text-sm font-medium text-black dark:text-black">Alamat</label>
-        <textarea id="address" disabled rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your description here" value=" {{ old('address', $data->address ?? '') }}"></textarea>
+        <textarea id="address" disabled rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 
+        focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+         placeholder="Your description here" >{{ old('address', $data->address ?? '') }}</textarea>
     </div>
   </div>
 </div>
